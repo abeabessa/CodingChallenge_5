60ba5da15456069d9d2392214da9abdff049a33c
abeabessa <bbessamarinho@gmail.com>
2024-09-21 22:42:31 -0400

    Initialize inventory with product objects

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+cc_5.js
+
+// Inventory Array of Product Objects
+const inventory = [
+  {name: 'Espresso', price: '2', quantity: '10'},
+  {name: 'Affogato', price: '6', quantity: '5'},
+  {name: 'Latte', price: '4', quantity: '5'},
+  {name: 'Cappucino', price: '4', quantity: '6'},
+];
